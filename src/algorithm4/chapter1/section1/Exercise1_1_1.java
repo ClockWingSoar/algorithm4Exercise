0322e7ebe4c5f56df166f788a17e4ad2181d28a4
ClockWingSoar <zyx568812@gmail.com>
2018-09-26 22:19:42 +0800

    add Exercise1_1_1 solution
    1.1.1 Give the value of each of the following expressions:
    a. ( 0 + 15 ) / 2
    b. 2.0e-6 * 100000000.1
    c. true && false || true && true

---
+++ src/algorithm4/chapter1/section1/Exercise1_1_1.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * 1.1.1 Give the value of each of the following expressions:
+	a. ( 0 + 15 ) / 2
+	b. 2.0e-6 * 100000000.1
+	c. true && false || true && true
+
+ */
+package algorithm4.chapter1.section1;
+
+/**
+ * @author YIXIANGZhong
+ *
+ */
+public class Exercise1_1_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int resultA = (0 + 15)/2;
+		double resultB = 2.0e-6 * 100000000.1;
+		boolean	 resultC = true && false || true && true;
+		
+		System.out.println("result A = " + resultA);
+		System.out.println("result B = " + resultB);
+		System.out.println("result C = " + resultC);
+		
+		
+		
+		
+		
+
+
+	}
+
+}
